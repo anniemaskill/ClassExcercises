963ee5d12a2f901f69c97a32b67596df893fcdca
Annie Maskill <maskilla@bc.edu>
2023-01-26 11:39:26 -0500

    Video 1.6 Done

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,31 +10,57 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString = "When the Genius bar needs help, they call you!"
     var body: some View {
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 1)
-                .padding()
-            HStack {
-                Button("Awesome") {
-                    // this is the action performed when the button is pressed
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
+        
+        ZStack {
+            Rectangle()
+                .fill(
+                    Gradient(colors: [Color("SkyBlue"), .indigo])
+                )
+                .ignoresSafeArea()
+    
+            VStack {
                 
-                Button("Great") {
-                    messageString = "You Are Great!"
+                Text("You Have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .cornerRadius(15)
+                
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 1)
+                    .padding()
+                
+                Spacer()
+
+                HStack {
+                    Button("Awesome") {
+                        // this is the action performed when the button is pressed
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
+    //            .border(.purple, width: 5)
+                .padding()
+                .tint(Color("Vermillion"))
             }
-            .border(.purple, width: 5)
-            .padding()
         }
     }
 }
